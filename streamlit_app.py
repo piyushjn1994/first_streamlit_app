426b910714b7726aab971f9b2c2d826b5927102e
piyushjn1994 <137996638+piyushjn1994@users.noreply.github.com>
2023-06-28 14:38:06 +0530

    Update streamlit_app.py
    added paratha

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-Streamlit.text('Paratha')
-Streamlit.text('Shake')
+streamlit.text('Paratha')
+
 
 
